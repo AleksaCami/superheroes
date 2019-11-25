57d39d40fa34de0f98c5e907521fe9ccb5ff32a3
Aleksa Camilovic <aleksa@utlimatemovers.net>
2019-11-25 19:06:11 +0100

    initial commit, firebase config

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+const configOptions = {
+  apiKey: "AIzaSyBqpKk1t-uG04f5fo5bJMQd4zg0pFiKgvg",
+  authDomain: "superheroes-6c832.firebaseapp.com",
+  databaseURL: "https://superheroes-6c832.firebaseio.com",
+  projectId: "superheroes-6c832",
+  storageBucket: "superheroes-6c832.appspot.com",
+  messagingSenderId: "1016552198415",
+  appId: "1:1016552198415:web:db1e8afee90eac2cf06e7a",
+  measurementId: "G-3RZ3VX1T9N"
+};
+
+firebase.initializeApp(configOptions);
+
+Vue.config.productionTip = false;
+
 new Vue({
   router,
   store,
